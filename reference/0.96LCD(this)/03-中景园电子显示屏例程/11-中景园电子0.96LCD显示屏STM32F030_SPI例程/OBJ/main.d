85c3fe8a25061b6573ef3ca0b0616606cbb119ff
xiaohua4758 <myway1199.qq.com>
2020-02-18 17:39:16 +0800

    add 0.96 tft-lcd references files

---
+++ reference/0.96LCD(this)/03-中景园电子显示屏例程/11-中景园电子0.96LCD显示屏STM32F030_SPI例程/OBJ/main.d
@@ -0,0 +1,15 @@
+..\obj\main.o: main.c
+..\obj\main.o: stm32f0xx.h
+..\obj\main.o: ..\CORE\core_cm0.h
+..\obj\main.o: D:\Program Files\mdk_arm_keil\ARM\ARMCC\Bin\..\include\stdint.h
+..\obj\main.o: ..\CORE\core_cmInstr.h
+..\obj\main.o: ..\CORE\core_cmFunc.h
+..\obj\main.o: system_stm32f0xx.h
+..\obj\main.o: stm32f0xx_conf.h
+..\obj\main.o: ..\STM32F03x_FWLib\inc\stm32f0xx_gpio.h
+..\obj\main.o: ..\User\stm32f0xx.h
+..\obj\main.o: ..\STM32F03x_FWLib\inc\stm32f0xx_rcc.h
+..\obj\main.o: ..\STM32F03x_FWLib\inc\stm32f0xx_usart.h
+..\obj\main.o: ..\STM32F03x_FWLib\inc\stm32f0xx_misc.h
+..\obj\main.o: ..\SYSTEM\delay.h
+..\obj\main.o: ..\HARDWARE\lcd.h
